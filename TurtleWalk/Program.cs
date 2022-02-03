414e88e406d046e235da62073b5b54f8bcdea6ab
Ignaz503 <lukasneuhold2@gmail.com>
2022-02-03 19:50:48 +0100

    auto sizing final image

--- TurtleWalk/Program.cs
+++ TurtleWalk/Program.cs
@@ -5,24 +5,47 @@ using SixLabors.ImageSharp.PixelFormats;
 using System.Collections;
 using System.Runtime.CompilerServices;
 
+WalkInfo walkInfo = new() { Base = 10, WalkSequence = new SpigotPi() { Count = 10_000 } };
 
-var t = new TurtleWalk();
-
-var visualizer = new TurtleWalkVisualizer<Argb32>(new Vector2(2560, 1440), Color.White);
-
-t.Walk(new() { Base = 10, WalkSequence = new SpigotPi() { Count = 10_000 } },
-    (i, step) =>
-    {
-        Console.WriteLine($"Step: {i}");
-        visualizer.DrawLine(step.PreviousPosition, step.CurrentPosition, Color.Black, 1f, 5f);
-    });
-var path = MakePath("spigot.png");
-visualizer.Save(path);
-
-Console.WriteLine($"saved at: {path}");
+//DrawDuringWalk("spigot.png",walkInfo, new Vector2(2560, 1440),new DrawInfo() { PixelPerUnit = 5f});
+DrawAfterWalk("spigotAfter.png",walkInfo, Vector2.One * 30,new DrawInfo() { PixelPerUnit = 5f });
 
 Console.WriteLine("Done");
 Console.ReadKey();
+
+void DrawAfterWalk(string name, WalkInfo walkInfo,Vector2 borderInPixel, DrawInfo drawInfo) 
+{
+    var t = new TurtleWalk();
+
+    var res = t.Walk(walkInfo,
+        (i) => Console.WriteLine($"Step Draw After: {i}"));
+
+    var visualizer = new TurtleWalkVisualizer<Argb32>(res,borderInPixel, drawInfo.ImgaeBGColor,drawInfo.LineColor,drawInfo.LineThickness,drawInfo.PixelPerUnit);
+    
+    var path = MakePath(name);
+    visualizer.Save(path);
+
+    Console.WriteLine($"saved at: {path}");
+}
+
+void DrawDuringWalk(string name,WalkInfo walkInfo,Vector2 imageSize, DrawInfo drawInfo) 
+{
+    var t = new TurtleWalk();
+
+    var visualizer = new TurtleWalkVisualizer<Argb32>(imageSize, drawInfo.ImgaeBGColor);
+
+    t.Walk(walkInfo,
+        (i, step) =>
+        {
+            Console.WriteLine($"Step Draw During Walk: {i}");
+            visualizer.DrawLine(step.PreviousPosition, step.CurrentPosition, drawInfo.LineColor,drawInfo.LineThickness,  drawInfo.PixelPerUnit);
+        });
+    var path = MakePath(name);
+    visualizer.Save(path);
+
+    Console.WriteLine($"saved at: {path}");
+}
+
 
 static string MakePath(string fileName)
     => $@"{Environment.CurrentDirectory}\{fileName}";
@@ -76,9 +99,23 @@ public class SpigotPi : IWalkSequence
 
         for (int i = 1; i <= Count; i++)
         {
-            yield return (Spigot(i), Turtle.PenState.Down);
+            var res = Spigot(i);
+
+            if (i == Count) //make it reuseable
+                Reset();
+
+            yield return (res, Turtle.PenState.Down);
         }
     }
+
+    void Reset() 
+    {
+        x = null;
+        r = null;
+        carry = 0;
+        prevDigit = 0;
+    }
+
     uint Spigot(int digit) 
         {
             if (this.x == null) 
@@ -126,4 +163,12 @@ public class SpigotPi : IWalkSequence
 
     IEnumerator IEnumerable.GetEnumerator()
         => GetEnumerator();
+}
+
+public struct DrawInfo 
+{
+    public Color ImgaeBGColor { get; init; } = Color.White;
+    public Color LineColor { get; init; } = Color.Black;
+    public float LineThickness { get; init; } = 1f;
+    public float PixelPerUnit { get; init; } = 50f;
 }
--- TurtleWalk/TurtleWalkVisualizer.cs
+++ TurtleWalk/TurtleWalkVisualizer.cs
@@ -17,19 +17,49 @@ public class TurtleWalkVisualizer<TPixel> where TPixel : unmanaged, IPixel<TPixe
         image = new Image<TPixel>(RoundToInt(size.X), RoundToInt(size.Y),bgColor.ToPixel<TPixel>());
     }
 
-    public void DrawLine(Vector2 start, Vector2 end, Color c, float thickness, float unitToPixels) 
+    public TurtleWalkVisualizer(TurtleWalkResult results, Vector2 borderInPixel, Color bgColor, Color lineColor, float lineThickness,float pixelPerUnit) 
+    {
+        var unscaledExtends = (results.Max - results.Min);
+        var extendsWithoutBorder =  unscaledExtends * pixelPerUnit;
+        var extends= extendsWithoutBorder + 2f * borderInPixel;
+
+
+
+        image = new Image<TPixel>(CeilToInt(extends.X), CeilToInt(extends.Y), bgColor.ToPixel<TPixel>());
+
+        var origin = borderInPixel;
+
+        for (int i = 0; i < results.Count-1; i++)
+        {
+            var start = results[i];
+            var end = results[i + 1];
+
+            start.X = Map(start.X, results.Min.X, results.Max.X, 0, unscaledExtends.X);
+            start.Y = Map(start.Y, results.Min.Y, results.Max.Y, 0, unscaledExtends.Y);
+
+            end.X = Map(end.X, results.Min.X, results.Max.X, 0, unscaledExtends.X);
+            end.Y = Map(end.Y, results.Min.Y, results.Max.Y, 0, unscaledExtends.Y);
+
+            DrawLine(start, end, lineColor, lineThickness, pixelPerUnit, origin);
+        }
+    }
+
+    void DrawLine(Vector2 start, Vector2 end, Color c, float thickness, float pixelPerUnit, Vector2 origin) 
     {
         var pathBuilder = new PathBuilder();
-        pathBuilder.SetOrigin(new PointF(image.Width / 2f, image.Height / 2f));
+        pathBuilder.SetOrigin(new PointF(origin.X, origin.Y));
 
-        start *= unitToPixels;
-        end *= unitToPixels;
+        start *= pixelPerUnit;
+        end *= pixelPerUnit;
 
         pathBuilder.AddLine(start.X, start.Y, end.X, end.Y);
 
         var path = pathBuilder.Build();
         image.Mutate(context => context.Draw(c, thickness, path));
     }
+
+    public void DrawLine(Vector2 start, Vector2 end, Color c, float thickness, float pixelPerUnit) 
+        => DrawLine(start, end, c, thickness, pixelPerUnit, new(image.Width / 2f, image.Height / 2f));
 
     public void Save(string filePath) 
     {
@@ -38,5 +68,11 @@ public class TurtleWalkVisualizer<TPixel> where TPixel : unmanaged, IPixel<TPixe
 
     static int RoundToInt(float val) 
         => (int)(val + 0.5f);
-    
+
+    static int CeilToInt(float val)
+        => (int)(val + 1f);
+    public static float Map(float x, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
 }
